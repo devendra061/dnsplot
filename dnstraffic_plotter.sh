20b3213bc290f92832370bfb4f464ce8c7e8c56c
Devendra <erdevendra@gmail.com>
2013-04-29 17:01:56 -0500

    Create dnstraffic_plotter.sh
    This script generates the data file "dns_log_analysisAllRecords" required to plot DNS traffic. 
    Then it uses "dns_plot_script.gp" to generate the graph as PNG image file. PNG image file can be viewed via any image viewer or internet browser.

---
+++ dnstraffic_plotter.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#This script will read bind.log file
+#Assumption is made that log file is located at /var/lib/named/var/lib/bind.log
+awk -F " |:" '{print $1" "$2":"$3}' /var/lib/named/var/lib/bind.log|uniq -c  > dns_log_analysisAllRecords
+
+gnuplot ./dns_plot_script.gp > dnstraffic.png
