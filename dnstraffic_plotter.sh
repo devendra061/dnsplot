c25b81ead545df62044df301ef53d694cdbf407f
Devendra <erdevendra@gmail.com>
2013-04-30 08:11:29 -0500

    Updated bind.log file location

--- dnstraffic_plotter.sh
+++ dnstraffic_plotter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #This script will read bind.log file
-#Assumption is made that log file is located at /var/lib/named/var/lib/bind.log
-awk -F " |:" '{print $1" "$2":"$3}' /var/lib/named/var/lib/bind.log|uniq -c  > dns_log_analysisAllRecords
+#Assumption is made that log file is located at /var/lib/named/var/log/bind.log
+awk -F " |:" '{print $1" "$2":"$3}' /var/lib/named/var/log/bind.log|uniq -c  > dns_log_analysisAllRecords
 
 gnuplot ./dns_plot_script.gp > dnstraffic.png
